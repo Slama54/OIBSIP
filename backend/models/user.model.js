5ca6097707b6b290dfb11040236be8ec98e43521
wael <wael.slama.issat@gmail.com>
2024-08-05 21:16:01 +0100

    create user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';    
+const userShema = new mongoose.Schema({
+    username:{
+        type :String,
+        required: true, 
+        unique: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    profilePicture:{
+        type: String,
+        default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    },
+    address:{
+        type: String,
+        required: true,
+    },
+    phone:{
+        type: String,
+        required: true,
+    },
+    status:{
+        type: String,
+        enum:['active', 'inactive'],
+        default: 'active',
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false,
+    }
+},{timestamps:true});
+const User = mongoose.model('User', userShema);
+export default User
