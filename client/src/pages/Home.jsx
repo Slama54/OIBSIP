6a3991a14ad1d0b48290fbf45815b3fa64277a7b
wael <wael.slama.issat@gmail.com>
2024-08-10 10:57:12 +0100

    create sign up page ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { Carousel } from 'flowbite-react'
+
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      
+      <div className=" h-screen sm:h-screen xl:h-screen 2xl:h-screen">
+      <Carousel > 
+        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+          Slide 1
+        </div>
+        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+          Slide 2
+        </div>
+        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+          Slide 3
+        </div>
+      </Carousel>
+    </div>
+
+    </div>
   )
 }
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,69 @@
+import { Button, Label, TextInput } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 export default function Signup() {
   return (
-    <div>Signup</div>
+   
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row
+      md:items-center gap-5">
+        {/*left*/}
+          <div className="flex-1">
+          <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+           <img src="\logo\pizza-hut-logo-DDD88793B2-seeklogo 2.png" className="mr-3 h-20 sm:h-36" alt="logo" />
+        </Link>
+        <p className="text-sm mt-5">
+          This is a demo projet you can sign up with your email 
+          and password or with google
+        </p>
+
+
+          </div>
+           {/*right*/}
+
+          <div className="flex-1">
+            <form className="flex flex-col gap-4" >
+               <div >
+                <Label value="Your username"></Label>
+                <TextInput type="text" placeholder="Username" id="username"/>
+
+               </div>
+               <div >
+                <Label value="Your address"></Label>
+                <TextInput type="text" placeholder="Address" id="address"/>
+
+               </div>
+               <div >
+                <Label value="Your phone number"></Label>
+                <TextInput type="number" placeholder="+216 " id="username"/>
+
+               </div>
+               <div >
+                <Label value="Your email"></Label>
+                <TextInput type="email" placeholder="email" id="email"/>
+
+               </div>
+               <div >
+                <Label value="Your password"></Label>
+                <TextInput type="password" placeholder="Password" id="password"/>
+
+               </div>
+               <Button gradientDuoTone='pinkToOrange' type="submit" >
+                Sign Up
+               </Button>
+
+            </form>
+            <div className=" flex gap-2 text-sm mt-5">
+              <span>Have an account ?</span>
+              <Link to='/sign-in' className="text-blue-500">
+                  Sign In
+              </Link>
+            </div>
+
+          </div>
+
+      </div>
+
+    </div>
   )
 }
