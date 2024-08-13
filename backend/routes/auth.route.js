1b8778ec84ba43f8cf1681d59ff6634c92849c74
wael <wael.slama.issat@gmail.com>
2024-08-13 10:33:17 +0100

    complete signin page functionality

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -6,5 +6,5 @@ import { signin, signup } from '../controllers/auth.controller.js';
 
  const router = express.Router();
  router.post('/signup', signup)
- router.post('/signup', signin)
+ router.post('/signin', signin)
  export default router
