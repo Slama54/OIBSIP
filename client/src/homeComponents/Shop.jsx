3db1b6184aeb7417b1b423e8628d160811a18010
wael <wael.slama.issat@gmail.com>
2024-08-14 22:05:00 +0100

    complete user API  route n controller and verify user

--- client/src/homeComponents/Shop.jsx
+++ client/src/homeComponents/Shop.jsx
@@ -4,7 +4,7 @@ export default function Shop() {
     <div className="w-full h-96 bg-cover bg-fixed bg-no-repeat bg-center relative" style={{ backgroundImage: `url('/bg.jpg')` }}>
     <div className="max-w-screen-xl mx-auto h-full flex items-center justify-start">
       <div className="text-center text-white">
-        <h1 className="text-xl w-5/6 md:text-2xl lg:text-4xl font-bold">Free Delivery With PIZZA OF THE DAY Only 8.99₹</h1>
+        <h1 className="text-xl w-5/6 md:text-2xl lg:text-4xl font-bold">Free Delivery With PIZZA OF THE DAY Only 2.9 dt</h1>
         <div className="mt-8 flex items-center justify-center">
           <a href="#" className="inline-block  px-10 py-4 text-sm font-bold text-white bg-red-600 rounded-full hover:bg-red-700 transition duration-300">
             SHOP NOW
