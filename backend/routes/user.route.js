da148e74bc647826bec5e80e8ec7c420a25225f3
wael <wael.slama.issat@gmail.com>
2024-08-15 12:06:52 +0100

    fixe error in user route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,7 +1,7 @@
 import express from 'express'
 
 import { deleteUser, getUser, getUsers, signout, test, updateUser } from '../controllers/user.controller.js';
-import { verifyToken } from '../utils/verifyuser.js';
+import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 router.get('/test',test);
 router.put('/update/:userId', verifyToken,updateUser);
