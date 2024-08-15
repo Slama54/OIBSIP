3052ee716934e509afe5c6f6eb1f2b0c513f7bdf
wael <wael.slama.issat@gmail.com>
2024-08-15 20:21:27 +0100

    add OAuth functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate  } from "react-router-dom";
 
 import { signInStart, signInFailure, signInSuccess } from "../redux/user/userSlice";
 import { useDispatch, useSelector } from "react-redux";
+import OAuth from "../components/OAuth";
 
 export default function Signin() {
   const [formData, setFormData] = useState({});
@@ -45,17 +46,14 @@ const {loading, error: errorMessage}=useSelector(state =>state.user)
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row
       md:items-center gap-5">
         {/*left*/}
-          <div className="flex-1">
-          <Link to="/" className="font-bold dark:text-white text-4xl">
-            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500
-             to-pink-500 rounded-lg text-white">Sahend </span>
-            Blog
+        <div className="flex-1">
+          <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+           <img src="\logo\pizza-hut-logo-DDD88793B2-seeklogo 2.png" className="mr-3 h-20 sm:h-36" alt="logo" />
         </Link>
         <p className="text-sm mt-5">
-          This is a demo projet you can sign in with your email 
+          This is a demo projet you can sign up with your email 
           and password or with google
         </p>
-
 
           </div>
            {/*right*/}
@@ -87,6 +85,7 @@ const {loading, error: errorMessage}=useSelector(state =>state.user)
                 'Sign Up'
               )}
             </Button>
+            <OAuth/>
 
             </form>
             <div className=" flex gap-2 text-sm mt-5">
