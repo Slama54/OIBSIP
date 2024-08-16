45827436f5e460830e0c41b38069e336a8b4f4a8
wael <wael.slama.issat@gmail.com>
2024-08-16 10:17:24 +0100

    fixe text color in the dark mode for about component

--- client/src/homeComponents/Abouth.jsx
+++ client/src/homeComponents/Abouth.jsx
@@ -4,13 +4,13 @@ export default function Abouth() {
   return (
     <div className="py-20">
     <div className="max-w-screen-xl mx-auto">
-      <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2 p-12">
-          <h3 className="text-gray-700 text-2xl font-bold mb-3">About Us</h3>
-          <h1 className="text-4xl lg:text-5xl font-extrabold text-gray-900 mb-5">
+      <div className="flex flex-wrap ">
+        <div className="w-full lg:w-1/2 p-12 ">
+          <h3 className="text-gray-700 text-2xl font-bold mb-3 dark:text-yellow-50">About Us</h3>
+          <h1 className="text-4xl lg:text-5xl font-extrabold text-gray-900 mb-5 dark:text-yellow-50">
             WELCOME TO PIZZA SHOP
           </h1>
-          <p className="text-lg text-gray-600 mb-8">
+          <p className="text-lg text-gray-600 dark:text-gray-300  mb-8">
             Investigationes demonstraverunt lectores legere me lius quod ii
             legunt saepius. Claritas est etiam processus dynaus, quise
             sequitur mutationem consuetudium lectorum.
