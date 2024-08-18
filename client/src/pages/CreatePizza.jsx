90cc12ce65464cac00834ab051891cb8ed5321dc
wael <wael.slama.issat@gmail.com>
2024-08-18 22:01:50 +0100

    complete upload pizza image functionality

--- client/src/pages/CreatePizza.jsx
+++ client/src/pages/CreatePizza.jsx
@@ -1,9 +1,60 @@
-import { Button, FileInput, Select, TextInput } from 'flowbite-react';
+import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import {  Label } from "flowbite-react";
+import { useState } from 'react';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 
 export default function CreatePizza() {
+  const [file, setFile]= useState(null)
+  const [formData, setFormData] = useState({});
+  const [imageUploadProgress,setImageUploadProgress] = useState(null)
+  const [imageUploadError, setImageUploadError]= useState(null)
+  const handleUpdloadImage = async () => {
+    try {
+      if (!file) {
+        setImageUploadError('Please select an image');
+        return;
+      }
+      setImageUploadError(null);
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + '-' + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageUploadProgress(progress.toFixed(0));
+        },
+        (error) => {
+          setImageUploadError('Image upload failed');
+          setImageUploadProgress(null);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUploadProgress(null);
+            setImageUploadError(null);
+            setFormData({ ...formData, image: downloadURL });
+          });
+        }
+      );
+    } catch (error) {
+      setImageUploadError('Image upload failed');
+      setImageUploadProgress(null);
+      console.log(error);
+    }
+  };
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
       <h1 className='text-center text-3xl my-7 font-semibold'>Create a pizza</h1>
@@ -21,6 +72,29 @@ export default function CreatePizza() {
             <option value='javascript'>basic</option>
             
           </Select>
+        </div>
+        <div className='flex flex-col gap-4 sm:flex-row justify-between'>
+          <TextInput
+            type='number'
+            placeholder='price size S'
+            required
+            id='price'
+            className='flex-1'
+          />
+          <TextInput
+            type='number'
+            placeholder='price size m'
+            required
+            id='price'
+            className='flex-1'
+          />
+          <TextInput
+            type='number'
+            placeholder='price size L'
+            required
+            id='price'
+            className='flex-1'
+          />
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-blue-200 border-dotted p-3'>
             
@@ -52,23 +126,42 @@ export default function CreatePizza() {
           </p>
           <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
         </div>
-        <FileInput id="dropzone-file" type='file' accept='image/*' className="hidden" />
+        <FileInput id="dropzone-file" type='file' accept='image/*' className="hidden" onChange={(e)=>setFile(e.target.files[0])} />
       </Label>
     </div>
 
 
 
          
-          <Button
-          className='w-fit'
+    <Button
             type='button'
             gradientDuoTone='purpleToBlue'
             size='sm'
             outline
+            onClick={handleUpdloadImage}
+            disabled={imageUploadProgress}
           >
-            Upload image
+            {imageUploadProgress ? (
+              <div className='w-16 h-16'>
+                <CircularProgressbar
+                  value={imageUploadProgress}
+                  text={`${imageUploadProgress || 0}%`}
+                />
+              </div>
+            ) : (
+              'Upload Image'
+            )}
           </Button>
         </div>
+        {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+        {formData.image && (
+          <img
+            src={formData.image}
+            alt='upload'
+            className='w-70 self-center  object-cover'
+          />
+        )
+        }
         <ReactQuill
           theme='snow'
           placeholder='Write something...'
