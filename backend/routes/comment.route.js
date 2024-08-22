9014d371bb026a3976ccdb5399bb0eeb9391bd45
wael <wael.slama.issat@gmail.com>
2024-08-22 21:17:41 +0100

    add like functionality to the comment component

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import {verifyToken} from'../utils/verifyuser.js'
-import {createComment, getPizzaComments} from '../controllers/comment.controller.js'
+import {createComment, getPizzaComments, likeComment} from '../controllers/comment.controller.js'
 const router = express.Router()
 router.post('/create', verifyToken,createComment)
 router.get('/getPizzaComments/:pizzaId', getPizzaComments)
+router.put('/likeComment/:commentId', verifyToken,likeComment)
 
 
 export default router;
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,13 +1,14 @@
 import { Alert, Button, Textarea } from 'flowbite-react'
 import { useEffect, useState } from 'react'
 import {useSelector} from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Comment from './Comment'
 export default function CommentSection({pizzaId}) {
     const {currentUser} = useSelector(state => state.user)
     const [comment, setComment] = useState('');
     const [commentError, setCommentError] = useState(null);
     const [comments, setComments] = useState([]);
+    const navigate = useNavigate()
     const handleSubmit = async (e)=>{
         e.preventDefault();
         if(comment.length > 200) {
@@ -56,7 +57,33 @@ useEffect(()=>{
   }
   getComments()
 },[pizzaId])
-
+const handleLike = async (commentId) => {
+  try {
+    if (!currentUser) {
+      navigate('/sign-in');
+      return;
+    }
+    const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+      method: 'PUT',
+    });
+    if (res.ok) {
+      const data = await res.json();
+      setComments(
+        comments.map((comment) =>
+          comment._id === commentId
+            ? {
+                ...comment,
+                likes: data.likes,
+                numberOfLikes: data.likes.length,
+              }
+            : comment
+        )
+      );
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
@@ -113,7 +140,7 @@ useEffect(()=>{
 
 
       </div>
-      {comments.map(comment => <Comment key={comment._id} comment={comment}/>)}
+      {comments.map(comment => <Comment key={comment._id} comment={comment} onLike={handleLike} /> )}
       </>
     )}
     </div>
