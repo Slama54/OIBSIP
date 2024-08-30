1f536fdbb8666e50dbaa0b4265a3a418dc7857b2
wael <wael.slama.issat@gmail.com>
2024-08-30 19:41:33 +0100

    update pizza cart

--- client/src/components/PizzaCard.jsx
+++ client/src/components/PizzaCard.jsx
@@ -1,34 +1,25 @@
-import { Card } from "flowbite-react";
+
 import { Link } from "react-router-dom";
 
 export default function PizzaCard({pizza}) {
   return (
     
     
-    <Card
-    className="group relative w-full max-w-xs rounded-lg sm:w-[430px] transition-all"
-   
-  >
-     <Link to={`/pizza/${pizza.slug}`}>
-    <img src={pizza.image} className="h-[320px]  w-full object-cover" alt="" />
-   
-      <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mt-4">
-        {pizza.title}
-      </h5>
-    </Link>
-    <div className="truncate  " dangerouslySetInnerHTML={{__html: pizza && pizza.content}}></div>
-    
-    <div className="flex items-center justify-between">
-      <span className="text-2xl font-bold text-gray-700 dark:text-gray-300">{pizza.priceS} dt</span>
-      <Link to ={`/pizza/${pizza.slug}`}
-        
-        className="rounded-lg bg-red-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-red-700 dark:hover:bg-red-800 dark:focus:ring-cyan-800"
-      >
-        order now
-      </Link>
-    </div>
-  </Card>
+    <div key={pizza._id} className="w-full lg:w-1/3 px-4 py-6">
+       <Link to={`/pizza/${pizza.slug}`}>
+    <div className="text-center">
+      <div className="mb-6">
+        <img src={pizza.image} alt={pizza.title} className="w-64 mx-auto" />
+      </div>
+      <h1 className="text-2xl font-semibold text-gray-900 mb-4 dark:text-gray-200">{pizza.title}</h1>
+      <div className="text-lg text-gray-600 dark:text-gray-400 mb-8 overflow-hidden" style={{ display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 2, lineClamp: 2, maxHeight: '4rem' }} dangerouslySetInnerHTML={{ __html: pizza && pizza.content }}></div>
 
+      <p className="text-2xl font-bold dark:text-gray-300 text-gray-800">{pizza.priceM}.000 DT</p>
+     
+    </div>
+    </Link>
+   
+  </div>
   
         
   )
